4b07df904cae6c8eb03968d8e5189cbff50c95c4
Büşra KIZILASLAN YALÇIN <busra_kizilaslan@hotmail.com>
2022-01-13 20:48:48 +0300

    Initial commit

---
+++ app/src/main/java/com/busrayalcin/artbook/Art.java
@@ -0,0 +1,12 @@
+package com.busrayalcin.artbook;
+
+public class Art {
+
+    String name ;
+    int id ;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
